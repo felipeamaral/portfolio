fd3f081dfd4a1a89534a37c0e7be00c7b2ecb660
Felipe Amaral <amaral.felipeaugusto@gmail.com>
2020-05-25 16:43:38 -0300

    adding flags and footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import Flags from 'country-flag-icons/react/3x2'
+
+import './Footer.css';
+import LOGO from './../../assets/images/favicon-32x32.png'
+const Footer = props => (
+    <footer className="footer-container">
+        <div className="footer-navigation">
+            <div className="footer-navigation-title">
+                <div className="footer-navigation-title-image">
+                    <img src={LOGO} />
+                </div>
+                <div className="footer-navigation-title-text">
+                    <p> by Felipe Amaral - 2020 </p>
+                </div>
+            </div>
+            <div className="spacer" />
+            <div className="footer-navigation-language">
+                <p>languages: </p>
+                <div className="footer-flag">
+                    <span className="footer-flag-left">
+                        <img
+                            alt="English"
+                            src="http://catamphetamine.gitlab.io/country-flag-icons/3x2/GB.svg"
+                        />
+                    </span>
+                    <span className="footer-flag-right">
+                        <img
+                            alt="Portugues"
+                            src="http://catamphetamine.gitlab.io/country-flag-icons/3x2/BR.svg"
+                        />
+                    </span>
+                    
+                </div>
+            </div>
+        </div>
+        
+        
+    </footer>
+)
+
+export default Footer;
