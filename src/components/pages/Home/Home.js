bda4610db6e77e2e0b3ce4db88071e90849037b5
Felipe Amaral <amaral.felipeaugusto@gmail.com>
2020-06-06 18:22:48 -0300

    adding some responsiveness

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -64,7 +64,7 @@ const Home = props => {
                     </div> 
                 </div>
                 <div className="pages-button-1">
-                    <Button color="blue" size='md' text={i18n.t("HOME_ABOUT_BUTTON")} click={goToAbout}/>    
+                    <Button color="blue" size='sm' text={i18n.t("HOME_ABOUT_BUTTON")} click={goToAbout}/>    
                 </div>
             </div>
             <div className="landing-page-2">
@@ -74,32 +74,20 @@ const Home = props => {
                     <IconText icon="ideate" title={i18n.t("HOME_PROCESS_DEFINE_TITLE")} text={i18n.t("HOME_PROCESS_DEFINE_TEXT")} />
                     <IconText icon="build" title={i18n.t("HOME_PROCESS_BUILD_TITLE")} text={i18n.t("HOME_PROCESS_BUILD_TEXT")} />
                     <IconText icon="refine" title={i18n.t("HOME_PROCESS_REFINE_TITLE")} text={i18n.t("HOME_PROCESS_REFINE_TEXT")} />
-                    {/* <div>
-                        <IconText icon="reasearch" title={i18n.t("HOME_PROCESS_RESEARCH_TITLE")} text={i18n.t("HOME_PROCESS_RESEARCH_TEXT")} />
-                    </div>
-                    <div>
-                        <IconText icon="ideate" title={i18n.t("HOME_PROCESS_DEFINE_TITLE")} text={i18n.t("HOME_PROCESS_DEFINE_TEXT")} />
-                    </div>
-                    <div>
-                        <IconText icon="build" title={i18n.t("HOME_PROCESS_BUILD_TITLE")} text={i18n.t("HOME_PROCESS_BUILD_TEXT")} />
-                    </div>
-                    <div>
-                        <IconText icon="refine" title={i18n.t("HOME_PROCESS_REFINE_TITLE")} text={i18n.t("HOME_PROCESS_REFINE_TEXT")} />
-                    </div> */}
                 </div>
                 <div className="pages-button-2">
-                    <Button color="yellow" size='md' text={i18n.t("HOME_PROCESS_BUTTON")} click={goToPortfolio}/>    
+                    <Button color="yellow" size='sm' text={i18n.t("HOME_PROCESS_BUTTON")} click={goToPortfolio}/>    
                 </div>
             </div>
-            {/* <div className="landing-page-3">
-                <h1 className="page-3-title"> Where do I work </h1>
+            <div className="landing-page-3">
+                <h1 className="page-3-title"> {i18n.t("HOME_INTRODUCTION_WORK_TITLE")} </h1>
                 <div>
-                    <p>...</p>
+
                 </div>
                 <div className="pages-button-3">
-                        <Button color="blue" size='md' text="my resume" click={goToResume}/>    
-                </div>
-            </div> */}
+                    <Button color="blue" size='sm' text={i18n.t("HOME_WORK_BUTTON")} click={goToAbout}/>    
+                </div>    
+            </div>
         </div>
     )
     
